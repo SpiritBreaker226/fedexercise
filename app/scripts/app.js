9b667eb0905ed1aa138aad0fca2329c911fb9067
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-06 20:06:53 -0400

    Init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fedexerciseApp
+ * @description
+ * # fedexerciseApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fedexerciseApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fedexerciseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fedexerciseApp
+ */
+angular.module('fedexerciseApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
