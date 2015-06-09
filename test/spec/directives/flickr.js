ddb11648e676a43b640d9eeb084e20d50201f605
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-09 18:09:50 -0400

    Remove Directive Flickr Test
    Remove directive Flickr test because it is too similar to the test for
    Flickr factory and does not add any additional information on the state
    of the application.

--- test/spec/directives/flickr.js
+++ test/spec/directives/flickr.js
@@ -5,16 +5,9 @@ describe('Directive: flickr', function () {
   // load the directive's module
   beforeEach(module('fedexerciseApp'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
-  }));
-
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<flickr></flickr>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the flickr directive');
   }));
 });
